0e92909205f03ca619a072a665cb9d0ab1d85af2
BigPasta30 <xk8pghpznf@privaterelay.appleid.com>
2026-02-02 20:17:00 +0000

    chore: first ownership edit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 32, fontFamily: "system-ui" }}>
+      <h1 style={{ marginBottom: 8 }}>Rich Teaches Music</h1>
+      <p style={{ marginTop: 0, opacity: 0.8 }}>
+        Music theory, taught while music is happening.
+      </p>
+    </main>
+  );
+}
